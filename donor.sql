d297be48ef8a5ae337df438eb51ec4e6c5ba2cba
akshayaerukulla <86769854+akshayaerukulla@users.noreply.github.com>
2022-12-08 11:53:08 +0530

    Add files via upload

---
+++ donor.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 08, 2022 at 07:21 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `donor`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `donor_details`
+--
+
+CREATE TABLE `donor_details` (
+  `donorname` text NOT NULL,
+  `donorid` int(10) NOT NULL,
+  `itemid` int(10) NOT NULL,
+  `address` varchar(30) NOT NULL,
+  `contactno` int(10) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `itemtype` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `donor_details`
+--
+ALTER TABLE `donor_details`
+  ADD PRIMARY KEY (`donorid`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
